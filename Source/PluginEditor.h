199e75e88c502adcb92e03880077595c54f6b8d5
Ross Martin <64719635+kingofdreams777@users.noreply.github.com>
2022-01-02 22:13:10 -0800

    analyzer button path is created only once

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -260,7 +260,29 @@ private:
 
 //==============================================================================
 struct PowerButton : juce::ToggleButton {};
-struct AnalyzerButton : juce::ToggleButton {};
+struct AnalyzerButton : juce::ToggleButton 
+{
+    void resized() override
+    {
+        auto bounds = getLocalBounds();
+        auto insetRect = bounds.reduced(4);
+
+        randomPath.clear();
+
+        juce::Random r;
+
+        randomPath.startNewSubPath(insetRect.getX(),
+            insetRect.getY() + insetRect.getHeight() * r.nextFloat());
+
+        for (auto x = insetRect.getX(); x < insetRect.getRight(); x += 2)
+        {
+            randomPath.lineTo(x,
+                insetRect.getY() + insetRect.getHeight() * r.nextFloat());
+        }
+    }
+
+    juce::Path randomPath;
+};
 /**
 */
 class SuperEQAudioProcessorEditor  : public juce::AudioProcessorEditor
