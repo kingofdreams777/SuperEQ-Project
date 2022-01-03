dceea2797cf510b50700c8f18892a14657e5ecb9
Ross Martin <64719635+kingofdreams777@users.noreply.github.com>
2022-01-02 16:21:54 -0800

    sliders turned into circles

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -48,7 +48,6 @@ void LookAndFeel::drawRotarySlider(juce::Graphics& g,
     p.applyTransform(AffineTransform().rotated(sliderAngRad, center.getX(), center.getY()));
 
     g.fillPath(p);
-
 }
 
 void RotarySliderWithLabels::paint(juce::Graphics& g)
@@ -60,6 +59,11 @@ void RotarySliderWithLabels::paint(juce::Graphics& g)
     auto range = getRange();
 
     auto sliderBounds = getSliderBounds();
+
+    g.setColour(Colours::red);
+    g.drawRect(getLocalBounds());
+    g.setColour(Colours::yellow);
+    g.drawRect(sliderBounds);
 
     getLookAndFeel().drawRotarySlider(g,
         sliderBounds.getX(),
@@ -74,7 +78,17 @@ void RotarySliderWithLabels::paint(juce::Graphics& g)
 
 juce::Rectangle<int> RotarySliderWithLabels::getSliderBounds() const
 {
-    return getLocalBounds();
+    auto bounds = getLocalBounds();
+
+    auto size = juce::jmin(bounds.getWidth(), bounds.getHeight());
+
+    size -= getTextHeight() * 2;
+    juce::Rectangle<int> r;
+    r.setSize(size, size);
+    r.setCentre(bounds.getCentreX(), 0);
+    r.setY(2);
+
+    return r;
 }
 //==============================================================================
 ResponseCurveComponent::ResponseCurveComponent(SuperEQAudioProcessor& p) : audioProcessor(p)
